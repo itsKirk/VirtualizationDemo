eae69320b52b1d4838e359a58e4a6130b4e16ff0
DrWambua <33222261+DrWambua@users.noreply.github.com>
2022-02-04 19:14:18 +0300

    Modified PersonService

--- Data/PersonService.cs
+++ Data/PersonService.cs
@@ -7,16 +7,15 @@
             "Dulce Leeming", "Fionnula	Gerbi", "Amy Melson", "Toby	Heskins", "Carri Adamczyk", "Barnie	Janssen", "Virgie Gong", "Ritchie Kellett", "Kassi	Attawell", "Arliene	Letch"
         };
 
-        public Task<Person[]> GetPeopleAsync()
+        public Task<Person[]> GetPeopleAsync(DateTime dateTime)
         {
             return Task.FromResult(Enumerable.Range(1, new Random().Next(500)).Select(index => new Person
             {
                 Id = index,
                 Name = Names[Random.Shared.Next(Names.Length)],
                 Gender = (Gender)new Random().Next(3),
-                Age = new Random().Next(18, 77)
-                
-            }).ToArray());
+                Age = dateTime.Year - new Random().Next(1955, 2022)
+            }).ToArray()); ;
         }
     }
 }
